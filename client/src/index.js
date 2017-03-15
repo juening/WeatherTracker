c5b0898d90fd45b25f96c3df5f1a9cc8af9f1171
Jue Ning <juening@gmail.com>
2017-03-15 14:18:38 -0400

    cient setup

---
+++ client/src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+
+import App from './components/app';
+import Header from './components/header';
+import reducers from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <Router history={browserHistory}>
+      <Route path="/" component={App} >
+        <Route path="signin" component={Header} />
+      </Route>
+    </Router>
+  </Provider>
+  , document.querySelector('.container'));
