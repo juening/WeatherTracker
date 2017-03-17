e0a70e55eff44adc2fc1cac67b7f3cdb0199f0f5
Jue Ning <juening@gmail.com>
2017-03-16 22:34:54 -0400

    automatically authenticate user

---
+++ client/src/components/homepage.js
@@ -0,0 +1,11 @@
+import React, { Component, PropTypes } from 'react';
+
+class HomePage extends Component {
+    render() {
+        return (
+            <h1>Welcome to Weather Tracker</h1>
+        );
+    }
+}
+
+export default HomePage;
--- client/src/index.js
+++ client/src/index.js
@@ -6,19 +6,28 @@ import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import reduxThunk from 'redux-thunk';
 
 import App from './components/app';
+import HomePage from './components/homepage';
 import Signin from './components/auth/signin';
 import Signout from './components/auth/signout';
 import Signup from './components/auth/signup';
 import Feature from './components/feature';
 import RequireAuth from './components/auth/requireAuth';
 import reducers from './reducers';
+import types from './actions/types';
 
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
+const store = createStoreWithMiddleware(reducers);
+
+const token = localStorage.getItem('token');
+if(token) {
+  store.dispatch({ type: types.AUTH_USER });
+}
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={store}>
     <Router history={browserHistory}>
       <Route path="/" component={App} >
+        <IndexRoute component={HomePage} />
         <Route path='signin' component={Signin} />
         <Route path='signout' component={Signout} />
         <Route path='signup' component={Signup} />
